8bdffa1879bc2e8adeea1d40ff3a1b50f5da7ffa
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2022-12-24 11:59:00 +0530

    orginn added

--- config/allowOrigins.js
+++ config/allowOrigins.js
@@ -1,3 +1,7 @@
-const allowOrigins = ["http://localhost:3000", "https://www.yourwebsite.com"];
+const allowOrigins = [
+  "http://localhost:3000",
+  "https://www.yourwebsite.com",
+  "https://test-deploy-client-teal.vercel.app/",
+];
 
 module.exports = allowOrigins;
